548e64f5c87700aefbd6c8eb9c67b3a5c6e1a43b
radwaw1 <166555572+radwaw1@users.noreply.github.com>
2026-07-05 22:16:13 +0100

    Update KrystalDisabler.lua

--- modules/KrystalDisabler.lua
+++ modules/KrystalDisabler.lua
@@ -5,18 +5,16 @@ KrystalDisabler.Name = "KrystalDisabler"
 KrystalDisabler.Run = function()
     task.wait(3)
     
-    print("KrystalDisabler: Starting...")
+    local KnitInit, Knit
+    repeat
+        KnitInit, Knit = pcall(function()
+            return require(game:GetService('ReplicatedStorage').rbxts_include.node_modules["@easy-games"].knit.src).KnitClient
+        end)
+        if KnitInit then break end
+        task.wait()
+    until KnitInit
 
-    local KnitInit, Knit = pcall(function()
-        return require(game:GetService('ReplicatedStorage').rbxts_include.node_modules["@easy-games"].knit.src).KnitClient
-    end)
-
-    if not KnitInit then
-        print("Failed to load Knit")
-        return
-    end
-
-    if not debug.getupvalue(Knit.Start, 1) then
+    if true and not debug.getupvalue(Knit.Start, 1) then
         repeat task.wait() until debug.getupvalue(Knit.Start, 1)
     end
 
@@ -29,25 +27,43 @@ KrystalDisabler.Run = function()
         end
     })
 
-    -- Only momentum bypass
-    local controller = bedwars.GlacialSkaterController
-    if controller then
-        local oldUpdate = controller.updateMomentum
-        hookfunction(oldUpdate, function(self, ...)
-            self.momentum = 9e9
-            self.lastMomentumReport = 9e9
-            pcall(function()
-                bedwars.Client:Get("MomentumUpdate"):SendToServer({ momentumValue = 9e9 })
-            end)
-        end)
-
+    -- Original Momentum Hook
+    local oldUpdateMomentum = bedwars.GlacialSkaterController.updateMomentum
+    hookfunction(oldUpdateMomentum, function(self, ...)
+        self.momentum = 9e9
+        self.lastMomentumReport = 9e9
         pcall(function()
-            controller:updateMomentum()
+            bedwars.Client:Get("MomentumUpdate"):SendToServer({
+                momentumValue = 9e9
+            })
         end)
-        print("Momentum bypass applied")
+    end)
+
+    pcall(function()
+        bedwars.GlacialSkaterController:updateMomentum()
+    end)
+
+    -- Selective SendToServer Hook (protects shop)
+    local momentumRemote = bedwars.Client:Get("MomentumUpdate")
+    if momentumRemote then
+        local oldSend = momentumRemote.SendToServer
+        hookfunction(oldSend, function(self, data)
+            -- Only intercept momentum packets
+            if type(data) == "table" and data.momentumValue ~= nil then
+                return oldSend(self, { momentumValue = 9e9 })
+            end
+
+            -- Protect shop remote
+            if self.Name == "Inventory/SetObservedChest" then
+                return oldSend(self, data)
+            end
+
+            -- Let all other remotes pass through
+            return oldSend(self, data)
+        end)
     end
 
-    print("✅ KrystalDisabler loaded (Safe)")
+    print("KrystalDisabler loaded (selective + shop protected)")
 end
 
 return KrystalDisabler
