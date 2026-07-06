376626cdbed8c88c0e396cb197dd3e6561f82645
radwaw1 <166555572+radwaw1@users.noreply.github.com>
2026-07-06 18:52:45 +0100

    Update Forcekrystal.lua

--- modules/Forcekrystal.lua
+++ modules/Forcekrystal.lua
@@ -1,12 +1,62 @@
-local ForceKrystal = {}
+local ForceKit = {}
 
-ForceKrystal.Name = "ForceKrystal"
-ForceKrystal.Enabled = false
+ForceKit.Name = "ForceKit"
+ForceKit.Enabled = false
 
+local player = game:GetService("Players").LocalPlayer
 
+ForceKit.Config = {
+    { 
+        Name = "Kit", 
+        Type = "Dropdown", 
+        Options = {
+            "aery", "agni", "airbender", "alchemist", "angel", "archer", "axolotl", 
+            "baker", "barbarian", "battery", "beast", "beekeeper", "berserker", 
+            "bigman", "black_market_trader", "block_kicker", "blood_assassin", 
+            "bounty_hunter", "builder", "cactus", "card", "cat", "conqueror", 
+            "cowgirl", "cyber", "dasher", "davey", "defender", "dino_tamer", 
+            "disruptor", "dragon_slayer", "dragon_sword", "drill", "elektra", 
+            "elk_master", "ember", "falconer", "farmer_cletus", "fisherman", 
+            "frosty", "frosty_hammer", "ghost_catcher", "gingerbread_man", 
+            "glacial_skater", "grim_reaper", "gun_blade", "hannah", "harpoon", 
+            "hatter", "ice_mage", "ice_queen", "ignis", "infected", 
+            "infected_disruptor", "infected_prowler", "infected_rush", 
+            "infected_tank", "jade", "jailor", "jellyfish", "lumen", "lyla", 
+            "mage", "melody", "merchant", "metal_detector", "midnight", "mimic", 
+            "miner", "nazar", "necromancer", "ninja", "nyoka", "oasis", 
+            "oil_man", "owl", "paladin", "pinata", "pyro", "queen_bee", 
+            "random", "raven", "rebellion_leader", "regent", "santa", "scarab", 
+            "seahorse", "sheep_herder", "shielder", "skeleton", "slime_tamer", 
+            "smoke", "sorcerer", "soul_broker", "spearman", "spider_queen", 
+            "spirit_assassin", "spirit_catcher", "spirit_gardener", 
+            "spirit_summoner", "star_collector", "steam_engineer", "styx", 
+            "summoner", "super_infected", "sword_shield", "taliyah", "tinker", 
+            "trapper", "triple_shot", "void_dragon", "void_hunter", 
+            "void_knight", "void_walker", "vulcan", "warlock", "warrior", 
+            "wind_walker", "wizard", "yeti"
+        }, 
+        Default = "random", 
+        Value = "random" 
+    }
+}
 
-ForceKrystal.Run = function()
-        game:GetService("ReplicatedStorage").rbxts_include.node_modules["@rbxts"].net.out._NetManaged.BedwarsActivateKit:InvokeServer({kit = 'glacial_skater'})
+ForceKit.Run = function()
+    ForceKit.Enabled = not ForceKit.Enabled
+
+    if ForceKit.Enabled then
+        print("✅ ForceKit Enabled - " .. ForceKit.Config[1].Value)
+        
+        -- Force the kit
+        local kitName = ForceKit.Config[1].Value
+        if kitName then
+            pcall(function()
+                game:GetService("ReplicatedStorage").TS.kit["bedwars-kit"].setKit:FireServer(kitName)
+            end)
+        end
+
+    else
+        print("❌ ForceKit Disabled")
+    end
 end
 
-return ForceKrystal
+return ForceKit
