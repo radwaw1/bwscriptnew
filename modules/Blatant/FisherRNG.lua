3670641d5bb0ac399c676a47a5ed2fad5231abe1
radwaw1 <166555572+radwaw1@users.noreply.github.com>
2026-07-08 18:10:58 +0100

    Update FisherRNG.lua

--- modules/Blatant/FisherRNG.lua
+++ modules/Blatant/FisherRNG.lua
@@ -1,6 +1,6 @@
 local FishingRNG = {}
 
-FishingRNG.Name = "Fishingman RNG"
+FishingRNG.Name = "FishingRNG"
 FishingRNG.Enabled = false
 
 FishingRNG.Run = function()
@@ -11,55 +11,39 @@ FishingRNG.Run = function()
 
         local FishFound = game:GetService("ReplicatedStorage").rbxts_include.node_modules["@rbxts"].net.out._NetManaged.FishFound
         local FishCaught = game:GetService("ReplicatedStorage").rbxts_include.node_modules["@rbxts"].net.out._NetManaged.FishCaught
-        local PullFishingRod = game:GetService("ReplicatedStorage").rbxts_include.node_modules["@rbxts"].net.out._NetManaged.PullFishingRod
 
-        -- Hook FishFound
+        -- Hook FishFound safely
         for _, conn in getconnections(FishFound.OnClientEvent) do
             if conn.Function then
-                local old = hookfunction(conn.Function, function(...)
-                    local args = table.pack(...)
-                    if args[1] and args[1].dropData then
-                        args[1].dropData = {
+                local old = hookfunction(conn.Function, function(data)
+                    if data and data.dropData then
+                        data.dropData = {
                             fishModel = "fish_gold",
                             weight = 5,
                             id = "gold_fish",
                             fishSizeMultiplier = 1.5,
-                            drops = args[1].dropData.drops  -- Keep original drops
+                            drops = data.dropData.drops or {}
                         }
                     end
-                    return old(...)
+                    return old(data)
                 end)
             end
         end
 
-        -- Hook FishCaught
+        -- Hook FishCaught safely
         for _, conn in getconnections(FishCaught.OnClientEvent) do
             if conn.Function then
-                local old = hookfunction(conn.Function, function(...)
-                    local args = table.pack(...)
-                    if args[1] and args[1].dropData then
-                        args[1].dropData = {
+                local old = hookfunction(conn.Function, function(data)
+                    if data and data.dropData then
+                        data.dropData = {
                             fishModel = "fish_gold",
                             weight = 5,
                             id = "gold_fish",
                             fishSizeMultiplier = 1.5,
-                            drops = args[1].dropData.drops  -- Keep original drops
+                            drops = data.dropData.drops or {}
                         }
                     end
-                    return old(...)
-                end)
-            end
-        end
-
-        -- Hook PullFishingRod to always succeed
-        for _, conn in getconnections(PullFishingRod.OnClientEvent) do
-            if conn.Function then
-                local old = hookfunction(conn.Function, function(...)
-                    local args = table.pack(...)
-                    if args[1] then
-                        args[1].success = true
-                    end
-                    return old(...)
+                    return old(data)
                 end)
             end
         end
