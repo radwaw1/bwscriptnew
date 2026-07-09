5ce23cb86dd8e50578c4b32cd6774ae98722c39c
radwaw1 <166555572+radwaw1@users.noreply.github.com>
2026-07-09 18:30:06 +0100

    Create Arrow1tapinf.lua

---
+++ modules/Blatant/Arrow1tapinf.lua
@@ -0,0 +1,84 @@
+local VolleyBow = {}
+
+VolleyBow.Name = "VolleyBow"
+VolleyBow.Enabled = false
+
+local replicated = game:GetService("ReplicatedStorage")
+local players = game:GetService("Players")
+local http = game:GetService("HttpService")
+local lplr = players.LocalPlayer
+
+local connection = nil
+
+VolleyBow.Config = {
+    { Name = "Keybind", Type = "Keybind", Value = Enum.KeyCode.RightControl }
+}
+
+local function getClosestRoot()
+    local distance, target = math.huge
+    for _, v in players:GetPlayers() do
+        if v ~= lplr and v.Team ~= lplr.Team then
+            local selfroot = lplr.Character and lplr.Character.PrimaryPart
+            local root = v.Character and v.Character.PrimaryPart
+            local compare = selfroot and root and (selfroot.Position - root.Position).Magnitude
+            if compare and distance > compare then
+                distance = compare
+                target = root
+            end
+        end
+    end
+    return target
+end
+
+local function getBow()
+    for _, v in replicated.Inventories[lplr.Name]:GetChildren() do
+        if v.Name:find('_bow') then
+            return v
+        end
+    end
+end
+
+VolleyBow.Run = function()
+    VolleyBow.Enabled = not VolleyBow.Enabled
+
+    if VolleyBow.Enabled then
+        print("✅ VolleyBow Enabled")
+        
+        connection = task.spawn(function()
+            while VolleyBow.Enabled do
+                local root = getClosestRoot()
+                local bow = getBow()
+                if root and bow then
+                    local selfroot = lplr.Character and lplr.Character.PrimaryPart
+                    if selfroot then
+                        replicated.rbxts_include.node_modules['@rbxts'].net.out._NetManaged.ProjectileFire:InvokeServer(
+                            bow,
+                            'volley_arrow',
+                            'arrow',
+                            selfroot.Position,
+                            selfroot.Position,
+                            (root.Position - selfroot.Position).Unit * (200 + (root.Position - selfroot.Position).Magnitude),
+                            http:GenerateGUID(false),
+                            {
+                                shotId = http:GenerateGUID(false),
+                                drawDurationSec = 9e9
+                            },
+                            workspace:GetServerTimeNow()
+                        )
+                    end
+                end
+
+                task.wait(0.1)
+            end
+        end)
+
+    else
+        print("❌ VolleyBow Disabled")
+        if connection then
+            connection:Disconnect()
+            connection = nil
+        end
+    end
+end
+
+return VolleyBow
